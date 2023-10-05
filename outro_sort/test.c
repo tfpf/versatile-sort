c424a886bdd75ce5f91637febd8883ffb977f016
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-10-05 22:43:05 +0530

    Improving the benchmarking code to report more representative running time. (#4)

--- outro_sort/test.c
+++ outro_sort/test.c
@@ -10,32 +10,75 @@ void insertion_sort(int *, int *);
 void outro_sort(int *, int *);
 
 /******************************************************************************
- * Check whether the sorting algorithm works correctly.
+ * Populate the array with random values.
  *
- * @param arr Array.
- * @param arr_size Number of elements in the array.
- * @param sorter Sort function.
+ * @param begin Pointer to the first element.
+ * @param end Pointer to one past the last element.
  *****************************************************************************/
 void
-test(int arr[], size_t arr_size, void (*sorter)(int *, int *))
+fill(int *begin, int *end)
 {
-    struct timespec begin, end;
-    clock_gettime(CLOCK_REALTIME, &begin);
-    sorter(arr, arr + arr_size);
-    clock_gettime(CLOCK_REALTIME, &end);
-    for(size_t i = 1; i < arr_size; ++i)
+    for(int *curr = begin; curr < end; ++curr)
     {
-        assert(arr[i - 1] <= arr[i]);
+        *curr = rand();
     }
+}
 
-    int tv_sec = end.tv_sec - begin.tv_sec;
-    int long tv_nsec = end.tv_nsec - begin.tv_nsec;
-    if(tv_nsec < 0)
+/******************************************************************************
+ * Check whether the array is sorted.
+ *
+ * @param begin Pointer to the first element.
+ * @param end Pointer to one past the last element.
+ *****************************************************************************/
+void
+verify(int const *begin, int const *end)
+{
+    for(int const *curr = begin + 1; curr < end; ++curr)
     {
-        --tv_sec;
-        tv_nsec += 1000000000L;
+        assert(*(curr - 1) <= *curr);
     }
-    printf("%d.%06ld s\n", tv_sec, tv_nsec / 1000L);
+}
+
+/******************************************************************************
+ * Check whether the sorting algorithm works correctly.
+ *
+ * @param sorter Sort function.
+ * @param arr_size Number of elements to sort.
+ *****************************************************************************/
+void
+test(void (*sorter)(int *, int *), size_t arr_size)
+{
+    int *arr = malloc(arr_size * sizeof *arr);
+    fill(arr, arr + arr_size);
+    sorter(arr, arr + arr_size);
+    verify(arr, arr + arr_size);
+    free(arr);
+}
+
+/******************************************************************************
+ * Measure the running time of the sorting algorithm.
+ *
+ * @param sorter Sort function.
+ * @param arr_size Number of elements to sort.
+ *****************************************************************************/
+void
+benchmark(void (*sorter)(int *, int *), size_t arr_size)
+{
+    int long long nanoseconds = 0;
+    int *arr = malloc(arr_size * sizeof *arr);
+    for(int i = 0; i < 16; ++i)
+    {
+        fill(arr, arr + arr_size);
+        struct timespec start, stop;
+        clock_gettime(CLOCK_REALTIME, &start);
+        sorter(arr, arr + arr_size);
+        clock_gettime(CLOCK_REALTIME, &stop);
+        nanoseconds += stop.tv_nsec - start.tv_nsec;
+        nanoseconds += 1000000000L * (stop.tv_sec - start.tv_sec);
+    }
+    free(arr);
+    nanoseconds /= 16;
+    printf("%.3lf ms\n", nanoseconds / 1000000.0);
 }
 
 /******************************************************************************
@@ -56,12 +99,7 @@ main(int const argc, char const *argv[])
     }
 
     srand(time(NULL));
-    int *arr = malloc(arr_size * sizeof *arr);
-    for(size_t i = 0; i < arr_size; ++i)
-    {
-        arr[i] = rand();
-    }
-    test(arr, arr_size, outro_sort);
-    free(arr);
+    test(outro_sort, arr_size);
+    benchmark(outro_sort, arr_size);
     return EXIT_SUCCESS;
 }
